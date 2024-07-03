d6b1b160dd98d4887d20a55a5fc267ee8970cf3e
gabri <gabri@LAPTOP-EGCVT37Q>
2024-07-03 20:30:29 -0300

    segundo commit

---
+++ tiposVariaveis/src/tiposVariaveis/Operadores.java
@@ -0,0 +1,11 @@
+package tiposVariaveis;
+
+public class Operadores {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nomeCompleto = "LINGUAGEM" + "JAVA";
+		System.out.println(nomeCompleto);
+	}
+
+}
